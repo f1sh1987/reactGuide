2ddd97fec51e7c5a61bfdcf6acbfc333311214b6
afischer <andreas.fischer@prodyna.com>
2018-08-20 09:33:20 +0200

    Update Component Lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,6 +27,21 @@ class App extends Component {
   componentDidMount(){
     console.log('[App.js] Inside componentDidMount()');
   }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return true;
+  }
+  
+  componentWillUpdate(nextProps, nextState){
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+  
+  componentDidUpdate(){
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
+
   //if state changes ==> re-render dom
 
 deletePerson = (personsIndex) => {
